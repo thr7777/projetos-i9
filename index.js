2bb223610377d5046a1118fd4b477655b2f88057
THR7777 <112986586+thr7777@users.noreply.github.com>
2026-08-10 19:52:22 -0300

    Primeiro perfil - Perfil próprio

---
+++ index.js
@@ -0,0 +1,45 @@
+console.log("funcionando");
+
+//mudando cor de link
+
+/* const link = document.getElementById("contato");
+
+link.addEventListener("mouseenter", ()=>{
+    //link.classList.add("ativo");
+    link.style.transform = "scale(1.05)"
+})
+link.addEventListener("mouseleave", ()=>{
+    //link.classList.remove("ativo");
+    link.style.transform = "scale(1)";
+}) */
+
+const contato = document.querySelectorAll(".contato");
+
+//como agora teremos uma lista, precisamos passar por cada um para conseguir modifica-los
+
+contato.forEach((contatos)=>{
+    contatos.addEventListener("mouseenter", ()=>{
+        console.log("ENTROU NO LINK");
+        contatos.style.transform = "scale(1.05)";
+    });
+
+    contatos.addEventListener("mouseleave", ()=>{
+        contatos.style.transform = "scale(1)";
+    });
+})
+
+const tema = document.getElementById("tema");
+
+const body = document.getElementById("b");
+const caixa = document.querySelectorAll(".caixa");
+
+tema.addEventListener("click", ()=>{
+    body.classList.toggle("mudar-tema");
+
+    caixa.forEach((caixas) =>{
+        caixas.classList.toggle("mudar-caixa");
+    })
+    contato.forEach((contatos)=>{
+        contatos.classList.toggle("mudar-cor-link");
+    })
+})
